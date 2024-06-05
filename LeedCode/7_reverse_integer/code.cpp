243caddf11744459778445502c6ad38bc6173d11
dakshinatharindu <tharinduokd@gmail.com>
2024-06-06 00:25:48 +0530

    complete quesion 7

--- LeedCode/7_reverse_integer/code.cpp
+++ LeedCode/7_reverse_integer/code.cpp
@@ -9,9 +9,25 @@ class Solution {
     int reverse(int x) {
         int reverse_x = 0;
         int digits = 0;
+        int temp;
+        bool pos = (x > 0) ? true : false;
 
         while (x != 0) {
-            reverse_x = reverse_x * 10 + (x % 10);
+            if (pos) {
+                if ((INT32_MAX / 10) < reverse_x) return 0;
+            } else {
+                if ((INT32_MIN / 10) > reverse_x) return 0;
+            }
+
+            reverse_x *= 10;
+            temp = (x % 10);
+            if (pos) {
+                if ((INT32_MAX - reverse_x) < temp) return 0;
+            } else {
+                if ((INT32_MIN - reverse_x) > temp) return 0;
+            }
+
+            reverse_x = reverse_x + temp;
             digits++;
             x = x / 10;
         }
@@ -22,5 +38,5 @@ class Solution {
 
 int main() {
     Solution s;
-    cout << s.reverse(2147483641) << endl;
+    cout << s.reverse(-123) << endl;
 }
