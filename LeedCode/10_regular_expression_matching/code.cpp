1676fe6c2738d519f5450030e8e36912c8116b9a
dakshinatharindu <dakshinatharindu99@gmail.com>
2025-05-06 11:34:24 -0400

    use dp method

--- LeedCode/10_regular_expression_matching/code.cpp
+++ LeedCode/10_regular_expression_matching/code.cpp
@@ -5,35 +5,38 @@ using namespace std;
 class Solution {
    public:
     bool isMatch(string s, string p) {
-        char prev;
-        int i = 0, j = 0;
-        while (j <= s.size()) {
-            if (i == p.size()) {
-                return false;
-            }
+        int m = s.size(), n = p.size();
 
-            if (p[i] == s[j] || p[i] == '.') {
-                prev = s[j];
-                i++;
-                continue;
+        bool dp[m + 1][n + 1];
+        for (int i = 0; i <= m; ++i) {
+            for (int j = 0; j <= n; ++j) {
+                dp[i][j] = false;
             }
+        }
+        dp[0][0] = true;
 
-            if (p[i] == '*') {
-                if (prev == s[j]) {
-                    j++;
-                    continue;
-                } else {
-                    i++;
-                    continue;
+        for (int j = 2; j <= n; j += 2) {
+            if (p[j - 1] == '*' && dp[0][j - 2]) dp[0][j] = true;
+        }
+
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (p[j - 1] == '.' || p[j - 1] == s[i - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i][j - 2]; 
+                    if (p[j - 2] == '.' || p[j - 2] == s[i - 1]) {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                    }
                 }
             }
         }
 
-        return true;
+        return dp[m][n];
     }
 };
 
 int main() {
     Solution s;
-    cout << s.isMatch("aa", "a*") << endl;
+    cout << s.isMatch("mississippi", "mis*is*p*.") << endl;
 }
