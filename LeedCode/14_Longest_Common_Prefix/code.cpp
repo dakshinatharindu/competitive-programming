0acf1eb337290ab1055fa45ce68b3c0d6433b771
dakshinatharindu <dakshinatharindu99@gmail.com>
2025-05-21 14:22:53 -0400

    initial solution

---
+++ LeedCode/14_Longest_Common_Prefix/code.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+   public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+
+        string prefix = strs[0];
+        for (int i = 1; i < strs.size(); ++i) {
+            while (strs[i].find(prefix) != 0) {
+                prefix = prefix.substr(0, prefix.size() - 1);
+                if (prefix.empty()) return "";
+            }
+        }
+        return prefix;
+    }
+};
+
+
+int main() {
+    Solution s;
+    vector<string> strs = {"flower", "flow", "flight"};
+    cout << s.longestCommonPrefix(strs) << endl;  // Output: "fl"
+}
