67b6c8a98ce6e91c40c5a11e3233a7c17f18ab54
dakshinatharindu <dakshinatharindu99@gmail.com>
2026-01-12 15:43:20 -0500

    complete the 22 problem

--- LeedCode/22_Generate_Parentheses/code.cpp
+++ LeedCode/22_Generate_Parentheses/code.cpp
@@ -6,9 +6,37 @@ using namespace std;
 
 class Solution {
    public:
-    vector<string> generateParenthesis(int n) {}
+    vector<string> output;
+
+    void goRecursive(int leftP, int rightP, string res, int stack) {
+        if ((leftP == 0) && (rightP == 0)) {
+            output.push_back(res);
+            return;
+        }
+
+        if (leftP != 0) {
+            goRecursive(leftP - 1, rightP, res + "(", stack + 1);
+        }
+        if (stack && (rightP != 0)) {
+            goRecursive(leftP, rightP - 1, res + ")", stack - 1);
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        goRecursive(n, n, "", 0);
+        return output;
+    }
 };
 
 int main() {
+    Solution solution;
+    int n = 3;
+    vector<string> result = solution.generateParenthesis(n);
 
+    cout << "Generated Parentheses combinations for n = " << n << ":\n";
+    for (const auto& combination : result) {
+        cout << combination << "\n";
+    }
+
+    return 0;
 }
